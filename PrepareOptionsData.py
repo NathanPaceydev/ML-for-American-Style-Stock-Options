c1fce0eeef6ec1a73b0a396b77038867e0f474ff
NathanPaceyDev <npacey01@gmail.com>
2023-11-11 18:26:54 -0500

    added combined testing with AMD / APPL

--- PrepareOptionsData.py
+++ PrepareOptionsData.py
@@ -4,81 +4,110 @@ import numpy as np
 import pandas as pd
 import os
 import sklearn
+
+import sys
+import io
+from contextlib import contextmanager
+import warnings
+
+@contextmanager
+def suppress_print():
+    original_stdout = sys.stdout
+    sys.stdout = io.StringIO()
+    try:
+        yield
+    finally:
+        sys.stdout = original_stdout
+
+
 #Function to produce training and targets using full dataset tables
 
 #GLOBAL PATH to data
 #DATA_PATH = '~/data/finance'
 
 def produceXYDataSets(ticker, corp, ns_back): 
-    df = pd.read_csv('~/data/finance'+'/optionchaindata/all/'+ticker+'_alldata_'+corp+'.csv.zip', parse_dates=['quoteDate','expiryDate'])
-    print("Dataframe shape from file",df.shape)
+    with suppress_print():
+        df = pd.read_csv('~/data/finance'+'/optionchaindata/all/'+ticker+'_alldata_'+corp+'.csv.zip', parse_dates=['quoteDate','expiryDate'])
     
-    #basic data cleaning, remove lines where the strike price is more than 50 from the stockprice
-    df_good = df[ df['strikeDelta'] > -50 ]
-    print("After removing deltastrike bigger than -50", df_good.shape)
+    # Print only the progress update
+    print(f"Processing {ticker} data...")
 
-    df_good = df_good[ df_good['strikeDelta'] < 50 ]
-    print("After removing deltastrike less than 50", df_good.shape)
+    with suppress_print():
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=pd.errors.SettingWithCopyWarning)
 
-    #All contract names to read through
-    contracts = df_good['contractSymbol'].unique()
+            df = pd.read_csv('~/data/finance'+'/optionchaindata/all/'+ticker+'_alldata_'+corp+'.csv.zip', parse_dates=['quoteDate','expiryDate'])
+            print("Dataframe shape from file",df.shape)
+            
+            #basic data cleaning, remove lines where the strike price is more than 50 from the stockprice
+            df_good = df[ df['strikeDelta'] > -50 ]
+            print("After removing deltastrike bigger than -50", df_good.shape)
 
-    x_train = []
-    y_train = []
+            df_good = df_good[ df_good['strikeDelta'] < 50 ]
+            print("After removing deltastrike less than 50", df_good.shape)
 
-    #Only look at contracts that have at least 1.5 time as many entries as the look back time
-    nquotes_min = int(1.5*ns_back)
-    good_contracts = []
+            #All contract names to read through
+            contracts = df_good['contractSymbol'].unique()
 
-    for contract in contracts:
-        data = df_good[df_good['contractSymbol'] == contract]
-        
-        if data.shape[0] < nquotes_min:
-            continue
-        
-        good_contracts.append(contract)
-        
-        data['deltaDays'] = data['quoteDate'].diff()
-        data['weekday'] = data['quoteDate'].dt.dayofweek
+            x_train = []
+            y_train = []
 
-        ndays = data.shape[0]
+            #Only look at contracts that have at least 1.5 time as many entries as the look back time
+            nquotes_min = int(1.5*ns_back)
+            good_contracts = []
 
-        for iday in range(ns_back+1, ndays):
-            #X:
-            stockPrices = data['stockClose'][iday-ns_back:iday].values
-            strike = data['strike'].values[iday]
-            openInterest = data['openInterest'].values[iday]
-            daysToExpiry = int(data['daysToExpiry'].values[iday].split('days')[0])
-            deltaDays = data['deltaDays'].values[iday]/ np.timedelta64(1, 'D')
-            weekday = data['weekday'].values[iday]
+            for contract in contracts:
+                data = df_good[df_good['contractSymbol'] == contract]
+                
+                if data.shape[0] < nquotes_min:
+                    continue
+                
+                good_contracts.append(contract)
+                
+                data['deltaDays'] = data['quoteDate'].diff()
+                data['weekday'] = data['quoteDate'].dt.dayofweek
 
-            features =np.concatenate( [[strike, openInterest, daysToExpiry, deltaDays, weekday], stockPrices] )
+                ndays = data.shape[0]
 
-            #y:
-            ask = data['ask'].values[iday]
-            bid = data['bid'].values[iday]
+                for iday in range(ns_back+1, ndays):
+                    #X:
+                    stockPrices = data['stockClose'][iday-ns_back:iday].values
+                    strike = data['strike'].values[iday]
+                    openInterest = data['openInterest'].values[iday]
+                    daysToExpiry = int(data['daysToExpiry'].values[iday].split('days')[0])
+                    deltaDays = data['deltaDays'].values[iday]/ np.timedelta64(1, 'D')
+                    weekday = data['weekday'].values[iday]
 
-            targets = np.array([bid, ask])
+                    features =np.concatenate( [[strike, openInterest, daysToExpiry, deltaDays, weekday], stockPrices] )
 
-            #print(weekday,ask,bid,daysToExpiry,deltaDays, strike,stock_prices)
-            x_train.append(features)
-            y_train.append(targets)
-    
+                    #y:
+                    ask = data['ask'].values[iday]
+                    bid = data['bid'].values[iday]
 
-    x_train = np.array(x_train)
-    y_train = np.array(y_train)
-    
-    print("Used",len(good_contracts),"contracts total")
-    print("Done, made data set with",x_train.shape[0],"samples")
-    
-    xydata = np.concatenate([x_train,y_train], axis=1)
-    DATA_PATH = os.path.expanduser('~/data/')  # Expands the ~ to the full home directory path
-    directory = os.path.join(DATA_PATH, 'save/')
+                    targets = np.array([bid, ask])
 
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-        
-    np.save(directory+ticker+'_'+corp+'_XY.npy',xydata)
+                    #print(weekday,ask,bid,daysToExpiry,deltaDays, strike,stock_prices)
+                    x_train.append(features)
+                    y_train.append(targets)
+            
+
+            x_train = np.array(x_train)
+            y_train = np.array(y_train)
+            
+            print("Used",len(good_contracts),"contracts total")
+            print("Done, made data set with",x_train.shape[0],"samples")
+            
+            xydata = np.concatenate([x_train,y_train], axis=1)
+            DATA_PATH = os.path.expanduser('~/data/')  # Expands the ~ to the full home directory path
+            directory = os.path.join(DATA_PATH, 'save/')
+
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+                
+            np.save(directory+ticker+'_'+corp+'_XY.npy',xydata)
+            
+    # Print completion message
+    print(f"Done processing {ticker}. Data set with {x_train.shape[0]} samples created.")
     
     return x_train, y_train
 
